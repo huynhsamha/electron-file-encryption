ff7dab682a45934e5074b33ba7adb747f74a14b0
DangPhuTrungTin <trungtincse@gmail.com>
2019-03-26 22:22:53 +0700

    error

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const {app, BrowserWindow} = require('electron')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let abc
 
 function createWindow () {
   // Create the browser window.
