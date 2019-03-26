18cd8d3157524f2abcd7ac18aa2a89a0315b5b3b
Anh. Nguyen Quoc (6) <anhnq6@vng.com.vn>
2019-03-27 01:03:24 +0700

    set limmit size for RSA algo

--- js/dashboard.js
+++ js/dashboard.js
@@ -162,7 +162,7 @@ $('#btnEncrypt').click(() => {
     console.log(password);
     if (!password || password == '' || password.length < 5) {
         enableButton()
-        return showAlert('Error', 'Passphrase is at least 5 charactersssss' + biggestSize)
+        return showAlert('Error', 'Passphrase is at least 5 characters' + biggestSize)
     }
 
     const files = treeView.files;
